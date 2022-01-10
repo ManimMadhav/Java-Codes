fe7694888f02ec421dbdff1e9ac2a7e3a896eb6c
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2022-01-10 13:12:27 +0530

    Update getDuration.java

--- getDuration.java
+++ getDuration.java
@@ -1,1 +1,20 @@
+public class methods{
+    public static void main(String[] args){
+      //another method
+      System.out.println(getDuration(500, 57)); 
 
+    }
+    
+    public static String getDuration(int minutes, int seconds){
+        if (minutes<0 || (seconds<0 && seconds>59)){
+            return "Invalid input.";
+        }
+
+        else{
+            int hours = minutes/60;
+            int min = minutes%60;
+
+            return (hours+" Hours, "+min+" minutes, "+seconds+" seconds.");
+        }
+    }
+}
