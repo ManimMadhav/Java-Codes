1593290507a31f36fdf3118556449a192a0bc409
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2022-01-13 20:26:21 +0530

    Update Main.java

--- Object Oriented Programming/Basic OOPs Exercises/simpleCalculatorExercise/Main.java
+++ Object Oriented Programming/Basic OOPs Exercises/simpleCalculatorExercise/Main.java
@@ -1,1 +1,16 @@
+package com.company;
 
+public class Main {
+
+    public static void main(String[] args) {
+	    simpleCalculator myObject = new simpleCalculator();
+        myObject.setFirstNumber(25);
+        myObject.setSecondNumber(10);
+
+        System.out.println("Sum is "+ myObject.getSum());
+        System.out.println("Difference is "+ myObject.getDifference());
+        System.out.println("Multiplication answer is "+ myObject.getMultiplied());
+        System.out.println("Division is "+ myObject.getDivided());
+
+    }
+}
