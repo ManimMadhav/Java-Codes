7b86f8bee6b146339de913996b0f900c55189f63
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2022-01-13 21:23:36 +0530

    Create Main.java

---
+++ Object Oriented Programming/Basic OOPs Exercises/personClassExercise/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        person myObject = new person();
+        myObject.setFirstName("Manim");
+        myObject.setSecondName("Madhav");
+        myObject.setAge(19);
+
+        System.out.println("Name is "+myObject.getFullName("Manim","Madhav"));
+        System.out.println("Is the person a teen? "+ myObject.isTeen(19));
+
+    }
+}
