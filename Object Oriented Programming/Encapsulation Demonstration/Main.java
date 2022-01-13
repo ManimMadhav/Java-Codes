298a335ba1137a3a736a3539d8e40922fe7147d3
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2022-01-13 19:49:14 +0530

    Update Main.java

--- Object Oriented Programming/Encapsulation Demonstration/Main.java
+++ Object Oriented Programming/Encapsulation Demonstration/Main.java
@@ -1,1 +1,10 @@
+package com.company;
 
+public class Main {
+
+    public static void main(String[] args) {
+	    Car lambo = new Car();
+        lambo.setModel("Galardo");
+        System.out.println(lambo.getModel());
+    }
+}
