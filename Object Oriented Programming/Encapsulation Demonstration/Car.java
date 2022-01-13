07f83538a96787112a477112b8c3f37160aed289
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2022-01-13 19:49:30 +0530

    Update Car.java

--- Object Oriented Programming/Encapsulation Demonstration/Car.java
+++ Object Oriented Programming/Encapsulation Demonstration/Car.java
@@ -1,1 +1,20 @@
+package com.company;
+
+public class Car {
+    private int doors;
+    private int wheels;
+    private String model;
+    private String colour;
+
+    //using a setter in Java
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    //using a getter in Java
+    public String getModel() {
+        return this.model;
+    }
+
+}
 
