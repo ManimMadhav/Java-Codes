dda0792f7a6ee1a0c265a6d6356c1b8b298c5c2f
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2022-01-13 20:26:02 +0530

    Update simpleCalculator.java

--- Object Oriented Programming/Basic OOPs Exercises/simpleCalculatorExercise/simpleCalculator.java
+++ Object Oriented Programming/Basic OOPs Exercises/simpleCalculatorExercise/simpleCalculator.java
@@ -1,1 +1,52 @@
+package com.company;
 
+public class simpleCalculator {
+    private double firstNumber;
+    private double secondNumber;
+
+    //setter
+    public void setFirstNumber(double firstNumber){
+        this.firstNumber = firstNumber;
+    }
+
+    //another setter
+    public void setSecondNumber(double secondNumber){
+        this.secondNumber = secondNumber;
+    }
+
+    //getter
+    public double getFirstNumber(){
+        return firstNumber;
+    }
+
+    //another getter
+    public double getSecondNumber(){
+        return secondNumber;
+    }
+
+    //the "behaviour" methods
+    public double getSum(){
+        double sum = firstNumber+secondNumber;
+        return sum;
+    }
+
+    public double getDifference(){
+        double difference = firstNumber-secondNumber;
+        return difference;
+    }
+
+    public double getMultiplied(){
+        double multiplied = firstNumber * secondNumber;
+        return multiplied;
+    }
+
+    public double getDivided(){
+        if (secondNumber !=0){
+            double divided = firstNumber/secondNumber;
+            return divided;
+        }
+        else {
+            return -1;
+        }
+    }
+}
