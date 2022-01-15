f9160bc1d3d7dc6afad6402c9502234bb9bc3fa4
Manim Madhav <66988314+ManimMadhav@users.noreply.github.com>
2022-01-15 23:55:09 +0530

    Update Main.java

--- Object Oriented Programming/Basic OOPs Exercises/accountingExercise/Main.java
+++ Object Oriented Programming/Basic OOPs Exercises/accountingExercise/Main.java
@@ -1,1 +1,13 @@
+//main
 
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args){
+        Accounting myObject = new Accounting();
+        System.out.println(myObject.depositFunds(500));
+        System.out.println(myObject.withdrawFunds(300));
+
+    }
+}
